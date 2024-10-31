1b22caa4cd90afac21c5b721fc830422f38c53ef
Diógenes Souza <diogenes.edusouza@gmail.com>
2024-10-30 23:00:01 -0300

    Update img
    Update img

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle('light')
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-    // if(html.classList.contains('light')) {
-    //     html.classList.remove('light')
-    // } else {
-    //     html.classList.add('light')
-    // }
+  // if(html.classList.contains('light')) {
+  //     html.classList.remove('light')
+  // } else {
+  //     html.classList.add('light')
+  // }
 
-    //Pegar a tag img
-    const img = document.querySelector('#profile img')
+  //Pegar a tag img
+  const img = document.querySelector("#profile img")
 
-    //Substituir a imagem
-    if(html.classList.contains('light')) {
-        //se tiver light mode, adicionar a imagem light
-        img.setAttribute("src", "./assets/avatar-diogenes-dark2.png")
-        img.setAttribute("alt", "Foto de Diógenes Souza de óculos escuro")
-    } else {
-        //se tiver sem light mode, manter a imagem normal
-        img.setAttribute('src', './assets/avatar-diogenes-dark2.png')
-        img.setAttribute('alt', 'Foto de Diógenes Souza de óculos escuro')
-    }
-
-
-
-    
-    
-    
-    
-    
+  //Substituir a imagem
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-diogenes-ligth-v1.png")
+    img.setAttribute("alt", "Foto de Diógenes Souza de óculos escuro")
+  } else {
+    //se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar-diogenes-dark-v1.png")
+    img.setAttribute("alt", "Foto de Diógenes Souza de óculos escuro")
+  }
 }
